2bf1153b23aac9ba50761b611002ff51f5ce820b
Fan Zhang <fzhang00@gmail.com>
2015-11-10 15:55:39 -0500

    1st commit display has vertical lines. might be related to pmp

---
+++ firmware/src/system_config/default/system_config.h
@@ -0,0 +1,186 @@
+/*******************************************************************************
+  MPLAB Harmony System Configuration Header
+
+  File Name:
+    system_config.h
+
+  Summary:
+    Build-time configuration header for the system defined by this MPLAB Harmony
+    project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    This configuration header must not define any prototypes or data
+    definitions (or include any files that do).  It only provides macro
+    definitions for build-time configuration options that are not instantiated
+    until used by another MPLAB Harmony module or application.
+    
+    Created with MPLAB Harmony Version 1.06
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2015 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef _SYSTEM_CONFIG_H
+#define _SYSTEM_CONFIG_H
+
+/* This is a temporary workaround for an issue with the peripheral library "Exists"
+   functions that causes superfluous warnings.  It "nulls" out the definition of
+   The PLIB function attribute that causes the warning.  Once that issue has been
+   resolved, this definition should be removed. */
+#define _PLIB_UNSUPPORTED
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/*  This section Includes other configuration headers necessary to completely
+    define this configuration.
+*/
+
+#include "bsp_config.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Service Configuration
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Common System Service Configuration Options
+*/
+#define SYS_VERSION_STR           "1.06"
+#define SYS_VERSION               10600
+
+// *****************************************************************************
+/* Clock System Service Configuration Options
+*/
+#define SYS_CLK_FREQ                        80000000ul
+#define SYS_CLK_BUS_PERIPHERAL_1            80000000ul
+#define SYS_CLK_UPLL_BEFORE_DIV2_FREQ       48000000ul
+#define SYS_CLK_CONFIG_PRIMARY_XTAL         8000000ul
+#define SYS_CLK_CONFIG_SECONDARY_XTAL       0ul
+   
+/*** Ports System Service Configuration ***/
+#define SYS_PORT_AD1PCFG        ~0xffdf
+#define SYS_PORT_CNPUE          0x0
+#define SYS_PORT_CNEN           0x0
+
+#define SYS_PORT_B_TRIS         0xffdf
+#define SYS_PORT_B_LAT          0x0
+#define SYS_PORT_B_ODC          0x0
+
+#define SYS_PORT_D_TRIS         0xfff8
+#define SYS_PORT_D_LAT          0x0
+#define SYS_PORT_D_ODC          0x0
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Configuration
+// *****************************************************************************
+// *****************************************************************************
+
+ #define  GFX_USE_DISPLAY_CONTROLLER_S1D13517
+
+/*** Graphics Display Configuration ***/
+
+
+#define DISP_ORIENTATION                        0
+#define DISP_HOR_RESOLUTION                     800
+#define DISP_VER_RESOLUTION                     480
+#define DISP_DATA_WIDTH			                24
+#define DISP_HOR_PULSE_WIDTH                    48
+#define DISP_HOR_BACK_PORCH                     88
+#define DISP_HOR_FRONT_PORCH                    40
+#define DISP_VER_PULSE_WIDTH                    3
+#define DISP_VER_BACK_PORCH                     32
+#define DISP_VER_FRONT_PORCH                    13
+#define DISP_INV_LSHIFT                         0
+#define GFX_LCD_TYPE                            GFX_LCD_TFT
+#define BACKLIGHT_ENABLE_LEVEL                  1
+#define BACKLIGHT_DISABLE_LEVEL                 0
+#define TCON_MODULE                             NULL
+// DOM-IGNORE-END
+
+#define USE_8BIT_PMP
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Middleware & Other Library Configuration
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+/*** GFX Library Configuration ***/
+
+#define GFX_INSTANCES_NUMBER                            1
+#define GFX_SELF_PREEMPTION_LEVEL                       0
+#define GFX_CONFIG_COLOR_DEPTH                          16
+
+#define GFX_CONFIG_FONT_CHAR_SIZE                       8
+#define GFX_CONFIG_FONT_EXTERNAL_DISABLE
+#define GFX_CONFIG_FONT_RAM_DISABLE
+#define GFX_CONFIG_IMAGE_FLASH_DISABLE
+#define GFX_CONFIG_IMAGE_EXTERNAL_DISABLE
+#define GFX_CONFIG_IMAGE_RAM_DISABLE
+#define GFX_CONFIG_RLE_DECODE_DISABLE
+#define GFX_CONFIG_IMAGE_PADDING_DISABLE
+#define GFX_CONFIG_ALPHABLEND_DISABLE
+#define GFX_CONFIG_GRADIENT_DISABLE
+#define GFX_CONFIG_PALETTE_DISABLE
+#define GFX_CONFIG_FONT_ANTIALIASED_DISABLE
+#define GFX_CONFIG_TRANSPARENT_COLOR_DISABLE
+#define GFX_CONFIG_PALETTE_EXTERNAL_DISABLE
+#define GFX_CONFIG_DOUBLE_BUFFERING_DISABLE
+#define GFX_CONFIG_USE_KEYBOARD_DISABLE
+#define GFX_CONFIG_USE_TOUCHSCREEN_DISABLE
+#define GFX_CONFIG_FOCUS_DISABLE
+#define GFX_malloc(size)                                    malloc(size)
+#define GFX_free(pObj)                                      free(pObj)
+
+
+// *****************************************************************************
+/* BSP Configuration Options
+*/
+#define BSP_OSC_FREQUENCY 8000000
+
+
+
+
+
+
+#endif // _SYSTEM_CONFIG_H
+/*******************************************************************************
+ End of File
+*/
+
